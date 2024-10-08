fcddea5dc5ef53604c33bd79163bc556c57ae4a8
nguyenthanhdat20032301 <datnt1@bsscommerce.com>
2024-10-08 15:13:37 +0700

    Problem: Remove Duplicates from Sorted Array 2

---
+++ problem/easy/Remove_Duplicates_from_Sorted_Array_2/Solution.java
@@ -0,0 +1,39 @@
+package problem.easy.Remove_Duplicates_from_Sorted_Array_2;
+
+class Solution {
+    // // Solution 1
+    // // Check if nums[i] = nums[i-2] -> shift all element from i + 1 to end to the left (= remove nums[i])
+    // // Time complexity: Bad case O(n^2)
+
+    // public int removeDuplicates(int[] nums) {
+    //     int i = 2; 
+    //     int length = nums.length;
+
+    //     while (i < length) {
+    //         if (nums[i] == nums[i-2]) {
+    //             for (int j = i; j < length - 1; j++) {
+    //                 nums[j] = nums[j+1];
+    //             }
+    //             length--;
+    //         } else {
+    //             i++; 
+    //         }
+    //     }
+
+    //     return length; 
+    // }
+
+    // Solution 2
+    // Two Pointer
+    public int removeDuplicates(int[] nums) {
+        int a = 2;
+        for (int b = 2; b < nums.length; b++) {
+            if(nums[b] != nums[a - 2]) {
+                nums[a] = nums[b];
+                a++;
+            }
+        }
+
+        return a;
+    }
+}
