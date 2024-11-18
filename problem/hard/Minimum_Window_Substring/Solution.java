89bc5fe532ddbe18364cd544a6c51b032d911122
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-11-18 14:44:20 +0700

    Solution: Minimum Window Substring

--- problem/hard/Minimum_Window_Substring/Solution.java
+++ problem/hard/Minimum_Window_Substring/Solution.java
@@ -1,1 +1,48 @@
 package problem.hard.Minimum_Window_Substring;
+
+import java.util.HashMap;
+
+class Solution {
+    public String minWindow(String s, String t) {
+        // Solution: Sliding Window + HashMap
+        // Time Complexity: O(m + n), traverse s -> m, store HashMap -> n
+        // Space Complexity: O(n), HashMap 
+        HashMap<Character, Integer> stored = new HashMap<>();
+        HashMap<Character, Integer> current = new HashMap<>();
+
+        for(char c : t.toCharArray()){
+            stored.put(c, stored.getOrDefault(c, 0) + 1);
+        }
+        
+        int firstIdx = 0, currentIdx = 0, start = 0, minLength = Integer.MAX_VALUE, count = 0;
+
+        while(currentIdx < s.length()){
+            char c = s.charAt(currentIdx);
+
+            if(stored.containsKey(c)){
+                current.put(c, current.getOrDefault(c, 0) + 1);
+                if(current.get(c) == stored.get(c)) count++;
+            }
+
+            while(count == stored.size()){
+                if(currentIdx - firstIdx + 1 < minLength){
+                    minLength = currentIdx - firstIdx + 1;
+                    start = firstIdx;
+                }
+
+                char firstChar = s.charAt(firstIdx);
+
+                if(stored.containsKey(firstChar)) {
+                    current.put(firstChar, current.get(firstChar) - 1);
+                    if(current.get(firstChar) < stored.get(firstChar)) count--;
+                }
+
+                firstIdx++;
+            }
+
+            currentIdx++;
+        }
+
+        return minLength == Integer.MAX_VALUE ? "" : s.substring(start, start + minLength);
+    }
+}
