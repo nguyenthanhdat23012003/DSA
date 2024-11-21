92dda95859b961804294dd41892fa2e9b32b0d0f
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-11-21 11:26:08 +0700

    Solution: Rotate Image

--- problem/medium/Rotate_Image/Solution.java
+++ problem/medium/Rotate_Image/Solution.java
@@ -1,1 +1,36 @@
 package problem.medium.Rotate_Image;
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i < j){
+                    crossSwap(matrix, i, j);
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(j < n / 2){
+                    verticalSwap(matrix, i, j);
+                }
+            }
+        }
+    }
+
+    public void crossSwap(int[][] matrix, int i, int j){
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+    }
+
+    public void verticalSwap(int[][] matrix, int i, int j){
+        int n = matrix.length;
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[i][n - j - 1];
+        matrix[i][n - j - 1] = temp;
+    }
+}
