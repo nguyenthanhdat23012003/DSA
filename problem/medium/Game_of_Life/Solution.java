f7bbc0d6093bd7e4b8394256dc51661a99cf8c4d
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-11-23 14:06:24 +0700

    Solution: Game Of Live Solution 1

--- problem/medium/Game_of_Life/Solution.java
+++ problem/medium/Game_of_Life/Solution.java
@@ -1,1 +1,38 @@
 package problem.medium.Game_of_Life;
+
+class Solution {
+    public void gameOfLife(int[][] board) {
+        // Solution 1: Copy matrix to count neighbors
+        // Time Complexity: O(m * n)
+        // Space Complexity: O(m * n)
+        int[][] expandBoard = new int[board.length + 2][board[0].length + 2];
+        for(int i = 0; i < expandBoard.length; i++){
+            for(int j = 0; j < expandBoard[0].length; j++){
+                if(i == 0 || j == 0 || i == expandBoard.length - 1 || j == expandBoard[0].length - 1){
+                    expandBoard[i][j] = 0;
+                } else {
+                    expandBoard[i][j] = board[i - 1][j - 1];
+                }
+            }
+        }
+
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                int sum = sumNeighbors(expandBoard, i + 1, j + 1);
+                if(board[i][j] == 1){
+                    if(sum != 2 && sum != 3){
+                        board[i][j] = 0;
+                    }
+                } else {
+                    if(sum == 3) board[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    private int sumNeighbors(int[][] board, int i, int j){
+        return board[i - 1][j - 1] + board[i - 1][j] + board[i - 1][j + 1]
+        + board[i][j - 1] + board[i][j + 1]
+        + board[i + 1][j - 1] + board[i + 1][j] + board[i + 1][j + 1];
+    }
+}
