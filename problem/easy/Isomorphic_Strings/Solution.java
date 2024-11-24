0a79d1144c493c6f0a30fa305efd342c2b60ed56
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-11-24 22:32:19 +0700

    Solution: Isomorphic Strings

--- problem/easy/Isomorphic_Strings/Solution.java
+++ problem/easy/Isomorphic_Strings/Solution.java
@@ -1,1 +1,22 @@
 package problem.easy.Isomorphic_Strings;
+
+import java.util.HashMap;
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> stored = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            if(stored.containsKey(sChar)){
+                if(stored.get(sChar) != tChar) return false;
+            } else if (stored.containsValue(tChar)){
+                return false;
+            } else {
+                stored.put(sChar, tChar);
+            }
+        }
+
+        return true;
+    }
+}
