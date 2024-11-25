6b3f515471b584adbcf14fec3b51b37e0ac968cf
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-11-25 09:12:53 +0700

    Solution: Valid Anagram - Solution 2

--- problem/easy/Valid_Anagram/Solution.java
+++ problem/easy/Valid_Anagram/Solution.java
@@ -1,19 +1,42 @@
 package problem.easy.Valid_Anagram;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // Solution 1: Sort and compare
+        // // Solution 1: Sort and compare
+        // if (s.length() != t.length()) {
+        //     return false;
+        // }
+        
+        // char[] sArray = s.toCharArray();
+        // char[] tArray = t.toCharArray();
+        // Arrays.sort(sArray);
+        // Arrays.sort(tArray);
+        
+        // return Arrays.equals(sArray, tArray);
+
+        // Solution 2: HashMap 
         if (s.length() != t.length()) {
             return false;
         }
         
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
-        Arrays.sort(sArray);
-        Arrays.sort(tArray);
+        HashMap<Character, Integer> charCountMap = new HashMap<>();
         
-        return Arrays.equals(sArray, tArray);
+        for (char c : s.toCharArray()) {
+            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+        }
+        
+        for (char c : t.toCharArray()) {
+            if (!charCountMap.containsKey(c)) {
+                return false; 
+            }
+            charCountMap.put(c, charCountMap.get(c) - 1);
+            if (charCountMap.get(c) == 0) {
+                charCountMap.remove(c); 
+            }
+        }
+        
+        return charCountMap.isEmpty();
     }
 }
