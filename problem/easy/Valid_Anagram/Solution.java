cba2e575c7f8edf70214c9363479fd86840087d0
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-11-25 09:11:20 +0700

    Solution: Valid Anagram - Solution 1

--- problem/easy/Valid_Anagram/Solution.java
+++ problem/easy/Valid_Anagram/Solution.java
@@ -1,1 +1,19 @@
 package problem.easy.Valid_Anagram;
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // Solution 1: Sort and compare
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        
+        return Arrays.equals(sArray, tArray);
+    }
+}
