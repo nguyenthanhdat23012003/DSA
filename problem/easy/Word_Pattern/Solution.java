2dccbfd880c57ab0e0a3fccb5483a091fc2d23e9
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-11-25 08:55:11 +0700

    Solution: Word Pattern

--- problem/easy/Word_Pattern/Solution.java
+++ problem/easy/Word_Pattern/Solution.java
@@ -1,1 +1,31 @@
 package problem.easy.Word_Pattern;
+
+import java.util.HashMap;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" "); 
+        if (pattern.length() != words.length) {
+            return false;
+        }
+        
+        HashMap<Character, String> stored = new HashMap<>();
+        
+        for (int i = 0; i < pattern.length(); i++) {
+            char patternChar = pattern.charAt(i);
+            String word = words[i];
+            
+            if (stored.containsKey(patternChar)) {
+                if (!stored.get(patternChar).equals(word)) {
+                    return false; 
+                }
+            } else if (stored.containsValue(word)) {
+                return false; 
+            } else {
+                stored.put(patternChar, word); 
+            }
+        }
+        
+        return true;
+    }
+}
