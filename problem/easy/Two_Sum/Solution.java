f503e090283db27e6ad2fd2348e36a0f50f06007
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-11-27 22:25:42 +0700

    Solution: Two Sum - Solution 1

--- problem/easy/Two_Sum/Solution.java
+++ problem/easy/Two_Sum/Solution.java
@@ -1,1 +1,20 @@
 package problem.easy.Two_Sum;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // Solution 1: Bruce Force
+        // Time Complexity: O(n^2)
+        // Space Complexity: O(n)
+        for(int i = 1; i < nums.length;i++){
+            for(int j = i;j<nums.length;j++){
+                if (nums[j] + nums[j-i] == target) {
+                    // ans[0] = j-i;
+                    // ans[1] = j;
+                    return new int[] {j-i,j};
+                }
+            }
+        }
+        
+        return new int[] {};
+    }
+}
