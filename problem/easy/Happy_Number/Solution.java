b0014f38e448d574f66439eb366c97cfffe52964
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-11-28 10:12:24 +0700

    Solution: Happy Number - Solution 1

--- problem/easy/Happy_Number/Solution.java
+++ problem/easy/Happy_Number/Solution.java
@@ -1,1 +1,28 @@
 package problem.easy.Happy_Number;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean isHappy(int n) {
+        // Solution 1: HashSet
+        Set<Integer> store = new HashSet<>();
+        while(n != 1 && !store.contains(n)){
+            store.add(n);
+            n = getSumOfSquares(n);
+        }
+
+        return n == 1;
+    }
+
+    private int getSumOfSquares(int n) {
+        int sum = 0;
+        while(n > 0){
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
