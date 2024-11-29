08ec58b0e4015394e75370317c6c954658319337
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-11-29 11:07:19 +0700

    Solution: Contains Duplicate II - Solution 2

--- problem/easy/Contains_Duplicate_II/Solution.java
+++ problem/easy/Contains_Duplicate_II/Solution.java
@@ -1,18 +1,30 @@
 package problem.easy.Contains_Duplicate_II;
-import java.util.HashMap;
 
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // Solution 1: HashMap
-        // Time Complexity: O(n)
-        // Space Complexity: O(n)
-        HashMap<Integer, Integer> stored = new HashMap<>();
+        // // Solution 1: HashMap
+        // // Time Complexity: O(n)
+        // // Space Complexity: O(n)
+        // HashMap<Integer, Integer> stored = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++){
-            if(stored.containsKey(nums[i]) && (i - stored.get(nums[i]) <= k)) return true;
-            stored.put(nums[i], i);
+        // for(int i = 0; i < nums.length; i++){
+        //     if(stored.containsKey(nums[i]) && (i - stored.get(nums[i]) <= k)) return true;
+        //     stored.put(nums[i], i);
+        // }
+
+        // return false;
+
+        // Solution 2: Sliding Window
+        // Time Complexity: O(n * k)
+        // Space Complexity: O(1)
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= i + k && j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
         }
-
         return false;
     }
 }
