15f7d1bb56147ea5f83aad55ec206fdde18534eb
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-11-29 10:47:44 +0700

    Solution: Contains Duplicate II - Solution 1

--- problem/easy/Contains_Duplicate_II/Solution.java
+++ problem/easy/Contains_Duplicate_II/Solution.java
@@ -1,1 +1,18 @@
 package problem.easy.Contains_Duplicate_II;
+import java.util.HashMap;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // Solution 1: HashMap
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+        HashMap<Integer, Integer> stored = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(stored.containsKey(nums[i]) && (i - stored.get(nums[i]) == k)) return true;
+            stored.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
