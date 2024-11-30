0a914bf90d7da86371e7cb3a44573c5f6e3a42c2
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-11-30 15:25:30 +0700

    Solution: Longest Consecutive Sequence

--- problem/medium/Longest_Consecutive_Sequence/Solution.java
+++ problem/medium/Longest_Consecutive_Sequence/Solution.java
@@ -1,1 +1,28 @@
 package problem.medium.Longest_Consecutive_Sequence;
+
+import java.util.HashSet;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> store = new HashSet<>();
+        int maxLength = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            store.add(nums[i]);
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(!store.contains(nums[i] - 1)){
+                int check = nums[i];
+                int currentMax = 1;
+                while(store.contains(check + 1)){
+                    currentMax++;
+                    check++;
+                }
+                if(currentMax > maxLength) maxLength = currentMax;
+            }
+        }
+
+        return maxLength;
+    }
+}
