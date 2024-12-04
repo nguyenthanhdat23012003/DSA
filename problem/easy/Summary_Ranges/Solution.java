08274a1956be31b3312fa249021c09c9a999d276
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-12-04 16:40:00 +0700

    Solution: Summary Ranges

--- problem/easy/Summary_Ranges/Solution.java
+++ problem/easy/Summary_Ranges/Solution.java
@@ -1,1 +1,38 @@
 package problem.easy.Summary_Ranges;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+
+        int start = nums[0]; // Start of the current range
+
+        for (int i = 1; i < nums.length; i++) {
+            // Check if the current number is not consecutive
+            if (nums[i] != nums[i - 1] + 1) {
+                // End the current range
+                if (start == nums[i - 1]) {
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(start + "->" + nums[i - 1]);
+                }
+                // Start a new range
+                start = nums[i];
+            }
+        }
+
+        // Add the last range
+        if (start == nums[nums.length - 1]) {
+            result.add(String.valueOf(start));
+        } else {
+            result.add(start + "->" + nums[nums.length - 1]);
+        }
+
+        return result;
+    }
+}
