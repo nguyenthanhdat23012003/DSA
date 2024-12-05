7c4da146158b659ff5e71b575a69621bc3bac182
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-12-05 09:48:39 +0700

    Solution: Merge Intervals

--- problem/medium/Merge_Intervals/Solution.java
+++ problem/medium/Merge_Intervals/Solution.java
@@ -1,1 +1,26 @@
 package problem.medium.Merge_Intervals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> result = new ArrayList<>();
+        int currentIdx = 0;
+        while(currentIdx < intervals.length - 1){
+            int nextIdx = currentIdx + 1;
+            if(intervals[currentIdx][1] >= intervals[nextIdx][0]){
+                intervals[nextIdx][0] = intervals[currentIdx][0];
+                intervals[nextIdx][1] = Math.max(intervals[currentIdx][1], intervals[nextIdx][1]);
+            }  else {
+                result.add(new int[]{intervals[currentIdx][0], intervals[currentIdx][1]});
+            }
+            currentIdx++;
+        }
+        result.add(new int[]{intervals[intervals.length - 1][0], intervals[intervals.length - 1][1]});
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
