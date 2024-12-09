2cdedf1d9babf289fccccb8532dc85d4dc0f1e52
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-12-09 15:25:33 +0700

    Solution Minimum Number of Arrows to Burst Balloons - Solution 1

--- problem/medium/Minimum_Number_of_Arrows_to_Burst_Balloons/Solution.java
+++ problem/medium/Minimum_Number_of_Arrows_to_Burst_Balloons/Solution.java
@@ -1,1 +1,22 @@
 package problem.medium.Minimum_Number_of_Arrows_to_Burst_Balloons;
+
+import java.util.Arrays;
+
+class Solution {
+    // Solution 1: Greedy
+    // Time Complexity: O(n * log(n))
+    // Space Complexity: O(1)
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+        int result = 1;
+        int end = points[0][1];
+        for(int i = 1; i < points.length; i++){
+            if(points[i][0] > end){
+                result++;
+                end = points[i][1];
+            }
+        }
+
+        return result;
+    }
+}
