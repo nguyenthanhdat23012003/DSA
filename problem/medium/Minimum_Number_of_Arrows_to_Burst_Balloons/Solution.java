384fe53a48158b9d4d9891e6ed46e3a855143cb1
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-12-09 15:34:38 +0700

    Solution Minimum Number of Arrows to Burst Balloons - Solution 2

--- problem/medium/Minimum_Number_of_Arrows_to_Burst_Balloons/Solution.java
+++ problem/medium/Minimum_Number_of_Arrows_to_Burst_Balloons/Solution.java
@@ -3,20 +3,38 @@ package problem.medium.Minimum_Number_of_Arrows_to_Burst_Balloons;
 import java.util.Arrays;
 
 class Solution {
-    // Solution 1: Greedy
+    // // Solution 1: Greedy
+    // // Time Complexity: O(n * log(n))
+    // // Space Complexity: O(1)
+    // public int findMinArrowShots(int[][] points) {
+    //     Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+    //     int result = 1;
+    //     int end = points[0][1];
+    //     for(int i = 1; i < points.length; i++){
+    //         if(points[i][0] > end){
+    //             result++;
+    //             end = points[i][1];
+    //         }
+    //     }
+
+    //     return result;
+    // }
+
+    // Solution 2: Interval Sweeping
     // Time Complexity: O(n * log(n))
     // Space Complexity: O(1)
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
         int result = 1;
-        int end = points[0][1];
+        int currentEnd = points[0][1];
         for(int i = 1; i < points.length; i++){
-            if(points[i][0] > end){
+            if(points[i][0] > currentEnd){
                 result++;
-                end = points[i][1];
+                currentEnd = points[i][1];
+            } else {
+                currentEnd = Math.min(currentEnd, points[i][1]);
             }
         }
-
         return result;
     }
 }
