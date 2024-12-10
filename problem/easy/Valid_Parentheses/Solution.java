6fe80886cd86882b901dfc5a875507c29ea89c6a
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-12-10 09:49:54 +0700

    Solution: Valid Parentheses

--- problem/easy/Valid_Parentheses/Solution.java
+++ problem/easy/Valid_Parentheses/Solution.java
@@ -1,1 +1,26 @@
 package problem.easy.Valid_Parentheses;
+
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stored = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '[' || c == '{'){
+                stored.push(c);
+            } else {
+                 if (stored.isEmpty()) {
+                    return false;
+                }
+                char top = stored.pop();
+                if ((c == ')' && top != '(') || 
+                    (c == '}' && top != '{') || 
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stored.isEmpty();
+    }
+}  
