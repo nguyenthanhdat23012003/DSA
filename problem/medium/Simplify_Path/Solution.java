b96f74a265c57c3dcd8fdc6389d0225c79842a77
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-12-11 11:55:55 +0700

    Solution: Simplify Path

--- problem/medium/Simplify_Path/Solution.java
+++ problem/medium/Simplify_Path/Solution.java
@@ -1,1 +1,27 @@
 package problem.medium.Simplify_Path;
+
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stored = new Stack<>();
+        String[] dirs = path.split("/");
+
+        for(String dir : dirs){
+            if(dir.equals(".") || dir.isEmpty()){
+                continue;
+            } else if (dir.equals("..")){
+                if(!stored.isEmpty()){
+                    stored.pop();
+                }
+            } else {
+                stored.push(dir);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(String dir : stored){
+            result.append("/").append(dir);
+        }
+
+        return result.length() > 0 ? result.toString() : "/";
+    }
+}
