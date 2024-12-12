aa77611e68a4e88eb28a9282a8bd736945988483
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-12-12 11:11:41 +0700

    Solution: Evaluate Reverse Polish Notation

--- problem/medium/Evaluate_Reverse_Polish_Notation/Solution.java
+++ problem/medium/Evaluate_Reverse_Polish_Notation/Solution.java
@@ -1,1 +1,37 @@
 package problem.medium.Evaluate_Reverse_Polish_Notation;
+
+import java.util.Stack;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        int result = 0;
+        Stack<String> stored = new Stack<>();
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("+")) {
+                int b = Integer.parseInt(stored.pop());
+                int a = Integer.parseInt(stored.pop());
+                result = a + b;
+                stored.push(Integer.toString(result));
+            } else if (tokens[i].equals("-")) {
+                int b = Integer.parseInt(stored.pop());
+                int a = Integer.parseInt(stored.pop());
+                result = a - b; 
+                stored.push(Integer.toString(result));
+            } else if (tokens[i].equals("*")) {
+                int b = Integer.parseInt(stored.pop());
+                int a = Integer.parseInt(stored.pop());
+                result = a * b;
+                stored.push(Integer.toString(result));
+            } else if (tokens[i].equals("/")) {
+                int b = Integer.parseInt(stored.pop());
+                int a = Integer.parseInt(stored.pop());
+                result = a / b; 
+                stored.push(Integer.toString(result));
+            } else {
+                stored.push(tokens[i]);
+            }
+        }
+
+        return Integer.parseInt(stored.peek());
+    }
+}
