f626e9f39fd533d537a8bb152d0b48fc6c9159e2
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-12-24 10:15:56 +0700

    Solution: Linked List Cycle - Solution 1

--- problem/easy/Linked_List_Cycle/Solution.java
+++ problem/easy/Linked_List_Cycle/Solution.java
@@ -1,1 +1,28 @@
 package problem.easy.Linked_List_Cycle;
+
+import java.util.HashSet;
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // Solution 1: HashSet
+        HashSet<ListNode> stored = new HashSet<>();
+        ListNode data = head;
+        while(data != null && data.next != null){
+            stored.add(data);
+            data = data.next;
+            if(stored.contains(data)) return true;
+        }
+
+        return false;     
+    }
+}
