033d692b23befd686bc8e1f77682768234fcb444
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-12-25 11:33:23 +0700

    Solution: Add Two Numbers - Solution 2

--- problem/medium/Add_Two_Numbers/Solution.java
+++ problem/medium/Add_Two_Numbers/Solution.java
@@ -11,37 +11,60 @@ class ListNode {
 
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // Solution 1: Combine numbers and then add -> not work with contraint, overflow.
-        int l1Num = 0, l2Num = 0;
-        int stepTime = 1;
+        // // Solution 1: Combine numbers and then add -> not work with contraint, overflow.
+        // int l1Num = 0, l2Num = 0;
+        // int stepTime = 1;
 
-        while(l1 != null){
-            l1Num += l1.val * stepTime;
-            stepTime *= 10;
-            l1 = l1.next;
+        // while(l1 != null){
+        //     l1Num += l1.val * stepTime;
+        //     stepTime *= 10;
+        //     l1 = l1.next;
+        // }
+
+        // stepTime = 1;
+
+        // while(l2 != null){
+        //     l2Num += l2.val * stepTime;
+        //     stepTime *= 10;
+        //     l2 = l2.next;
+        // }
+
+        // int resultInt = l1Num + l2Num;
+
+        // if(resultInt == 0) return new ListNode(0);
+
+        // ListNode dummyResult = new ListNode(0);
+        // ListNode stored = dummyResult;
+
+        // while(resultInt != 0){
+        //     stored.next = new ListNode(resultInt % 10);
+        //     resultInt = resultInt / 10;
+        //     stored = stored.next;
+        // }
+
+        // return dummyResult.next;
+
+        // Solution 2: Add each node to one
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
         }
 
-        stepTime = 1;
-
-        while(l2 != null){
-            l2Num += l2.val * stepTime;
-            stepTime *= 10;
-            l2 = l2.next;
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
         }
 
-        int resultInt = l1Num + l2Num;
-
-        if(resultInt == 0) return new ListNode(0);
-
-        ListNode dummyResult = new ListNode(0);
-        ListNode stored = dummyResult;
-
-        while(resultInt != 0){
-            stored.next = new ListNode(resultInt % 10);
-            resultInt = resultInt / 10;
-            stored = stored.next;
-        }
-
-        return dummyResult.next;
+        return dummyHead.next;
     }
 }
