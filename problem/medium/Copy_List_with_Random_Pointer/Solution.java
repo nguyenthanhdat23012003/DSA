33b4cbb2531fa3c14b89ace81420739b4c61bd64
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-12-30 11:18:34 +0700

    Solution: Copy List with Random Pointer - Solution 2

--- problem/medium/Copy_List_with_Random_Pointer/Solution.java
+++ problem/medium/Copy_List_with_Random_Pointer/Solution.java
@@ -1,5 +1,7 @@
 package problem.medium.Copy_List_with_Random_Pointer;
 
+import java.util.HashMap;
+
 // Definition for a Node.
 class Node {
     int val;
@@ -15,35 +17,62 @@ class Node {
 
 public class Solution {
     public Node copyRandomList(Node head) {
-        // Solution 1: Interleave node
+        // // Solution 1: Interleave node
+        // // Time Complexity: O(n)
+        // // Space Complexity: O(1)
+        // Node curr = head;
+        // while(curr != null){
+        //     Node copy = new Node(curr.val);
+        //     copy.next = curr.next;
+        //     curr.next = copy;
+        //     curr = copy.next;
+        // }
+
+        // curr = head;
+        // while (curr != null) {
+        //     if (curr.random != null) {
+        //         curr.next.random = curr.random.next;
+        //     }
+
+        //     curr = curr.next.next;
+        // }
+
+        // curr = head;
+        // Node dummy = new Node(0);
+        // Node copyNode = dummy;
+
+        // while(curr != null){
+        //     copyNode.next = curr.next;
+        //     curr.next = curr.next.next;
+        //     curr = curr.next;
+        //     copyNode = copyNode.next;
+        // }
+
+        // return dummy.next;
+
+        // Solution 2: HashMap
         // Time Complexity: O(n)
-        // Space Complexity: O(1)
+        // Space Complexity: O(n)
+        if (head == null) return null;
+
         Node curr = head;
-        while(curr != null){
-            Node copy = new Node(curr.val);
-            copy.next = curr.next;
-            curr.next = copy;
-            curr = copy.next;
-        }
-
-        curr = head;
-        while (curr != null) {
-            if (curr.random != null) {
-                curr.next.random = curr.random.next;
-            }
-
-            curr = curr.next.next;
-        }
-
-        curr = head;
+        HashMap<Node, Node> randomStored = new HashMap<>();
         Node dummy = new Node(0);
         Node copyNode = dummy;
 
-        while(curr != null){
-            copyNode.next = curr.next;
-            curr.next = curr.next.next;
+        while(curr != null) {
+            copyNode.next = new Node(curr.val);  
+            copyNode = copyNode.next;           
+            randomStored.put(curr, copyNode);   
             curr = curr.next;
-            copyNode = copyNode.next;
+        }
+
+        curr = head;
+        Node copy = dummy.next;
+        while(curr != null) {
+            copy.random = randomStored.get(curr.random); 
+            curr = curr.next;
+            copy = copy.next;
         }
 
         return dummy.next;
