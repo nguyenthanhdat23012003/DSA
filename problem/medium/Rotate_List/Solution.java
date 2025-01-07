c651cae3ea890964420694f41eebbe9d04855a27
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2025-01-07 11:00:23 +0700

    Solution: Rotate List

--- problem/medium/Rotate_List/Solution.java
+++ problem/medium/Rotate_List/Solution.java
@@ -1,1 +1,39 @@
 package problem.medium.Rotate_List;
+
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) return head;
+        
+        ListNode curr = head;
+        int count = 1;
+        while(curr.next != null){
+            count++;
+            curr = curr.next;
+        }
+
+        int realStep = k % count;
+
+        int lastIdx = count - realStep;
+        curr.next = head;
+
+        while(lastIdx > 0){
+            curr = curr.next;
+            lastIdx--;
+        }
+
+        head = curr.next;
+        curr.next = null;
+
+        return head;
+    }
+}
