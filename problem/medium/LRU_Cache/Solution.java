70c674b725d7c4881a4eec8d50ccd00300d13315
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2025-01-09 15:16:40 +0700

    Solution: LRU Cache

--- problem/medium/LRU_Cache/Solution.java
+++ problem/medium/LRU_Cache/Solution.java
@@ -1,1 +1,34 @@
 package problem.medium.LRU_Cache;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class Solution {
+    private final int capacity;
+    private final Map<Integer, Integer> cache;
+
+    public Solution(int capacity) {
+        this.capacity = capacity;
+        this.cache = new LinkedHashMap<>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        cache.put(key, value); 
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
