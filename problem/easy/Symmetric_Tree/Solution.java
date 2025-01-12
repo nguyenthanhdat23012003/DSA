fb2c3ccfd1aa6a5869a84effe49917d3f6e3ec90
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2025-01-12 21:51:49 +0700

    Solution: Symmetric Tree

--- problem/easy/Symmetric_Tree/Solution.java
+++ problem/easy/Symmetric_Tree/Solution.java
@@ -1,1 +1,31 @@
 package problem.easy.Symmetric_Tree;
+
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    // Solution 1: Recursive
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root.left, root.right);
+    }
+
+    public boolean isMirror(TreeNode left, TreeNode right){
+        if(left == null && right == null) return true;
+
+        if(left == null || right == null) return false;
+
+        return (left.val == right.val) && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+}
