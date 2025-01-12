799bdd10c4648865d50657e9591e4a68cb80e681
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2025-01-12 20:22:24 +0700

    Solution: Same Tree

--- problem/easy/Same_Tree/Solution.java
+++ problem/easy/Same_Tree/Solution.java
@@ -1,1 +1,25 @@
 package problem.easy.Same_Tree;
+
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(q == null && p == null) return true;
+
+        if(q == null || p == null) return false;
+        return (q.val == p.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
