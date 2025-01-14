fb08b111b86dfdaadd1f3d3a7717303c94514f05
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2025-01-14 10:44:46 +0700

    Solution: Construct Binary Tree from Preorder and Inorder Traversal

--- problem/medium/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/Solution.java
+++ problem/medium/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/Solution.java
@@ -1,1 +1,41 @@
 package problem.medium.Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal;
+
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTreeHelper(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+    }
+
+    private TreeNode buildTreeHelper(int[] preorder, int[] inorder, int preStart, int preEnd, int inStart, int inEnd) {
+        if(preStart > preEnd || inStart > inEnd) return null;
+        int rootVal = preorder[preStart];
+        TreeNode root = new TreeNode(rootVal);
+
+        int rootIdxIn = 0;
+        for(int i = inStart; i <= inEnd; i++){
+            if(inorder[i] == rootVal) rootIdxIn = i;
+        }
+
+        int leftSizeSubTree = rootIdxIn - inStart;
+
+        root.left = buildTreeHelper(preorder, inorder, preStart + 1, preEnd + leftSizeSubTree, inStart, rootIdxIn - 1);
+
+        root.right = buildTreeHelper(preorder, inorder, preStart + 1 + leftSizeSubTree, preEnd, rootIdxIn + 1, inEnd);
+
+        return root;
+    }
+}
