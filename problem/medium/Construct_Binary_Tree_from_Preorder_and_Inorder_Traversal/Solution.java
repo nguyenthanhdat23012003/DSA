f3a68c1b4544d053429a65c8c916bab1e4778c97
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2025-01-15 18:35:09 +0700

    Solution: Construct Binary Tree from Preorder and Inorder Traversal - Solution 2

--- problem/medium/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/Solution.java
+++ problem/medium/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/Solution.java
@@ -1,5 +1,6 @@
 package problem.medium.Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal;
 
+import java.util.HashMap;
 
 // Definition for a binary tree node.
 class TreeNode {
@@ -17,7 +18,32 @@ class TreeNode {
 
 public class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return buildTreeHelper(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+        // Solution 1: Iterative
+        // return buildTreeHelper(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+
+        // Solution 2: HashMap
+        HashMap<Integer, Integer> storeInOrder = new HashMap<>();
+        for(int i = 0; i < inorder.length; i++) {
+            storeInOrder.put(inorder[i], i);
+        }
+
+        return buildTreeHelperHash(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1, storeInOrder);
+    }
+
+    private TreeNode buildTreeHelperHash(int[] preorder, int[] inorder, int preStart, int preEnd, int inStart, int inEnd, HashMap<Integer, Integer> storeInOrder) {
+        if(preStart > preEnd || inStart > inEnd) return null;
+        int rootVal = preorder[preStart];
+        TreeNode root = new TreeNode(rootVal);
+
+        int rootIdxIn = storeInOrder.get(rootVal);
+
+        int leftSizeSubTree = rootIdxIn - inStart;
+
+        root.left = buildTreeHelper(preorder, inorder, preStart + 1, preEnd + leftSizeSubTree, inStart, rootIdxIn - 1);
+
+        root.right = buildTreeHelper(preorder, inorder, preStart + 1 + leftSizeSubTree, preEnd, rootIdxIn + 1, inEnd);
+
+        return root;
     }
 
     private TreeNode buildTreeHelper(int[] preorder, int[] inorder, int preStart, int preEnd, int inStart, int inEnd) {
