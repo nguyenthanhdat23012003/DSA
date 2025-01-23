224f37a99c0b8807a47b8f7f864149685b8823b6
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2025-01-23 13:46:40 +0700

    Solution: Binary Tree Maximum Path Sum

--- problem/hard/Binary_Tree_Maximum_Path_Sum/Solution.java
+++ problem/hard/Binary_Tree_Maximum_Path_Sum/Solution.java
@@ -1,1 +1,37 @@
 package problem.hard.Binary_Tree_Maximum_Path_Sum;
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    private int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        maxGain(root);
+        return maxSum;
+    }
+
+    private int maxGain(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftGain = Math.max(maxGain(node.left), 0);
+        int rightGain = Math.max(maxGain(node.right), 0);
+
+        int priceNewPath = node.val + leftGain + rightGain;
+
+        maxSum = Math.max(maxSum, priceNewPath);
+
+        return node.val + Math.max(leftGain, rightGain);
+    }
+}
