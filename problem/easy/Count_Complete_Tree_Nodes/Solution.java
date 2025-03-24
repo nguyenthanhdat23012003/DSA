20aa6100cec0964719c10e9b3fd44636e27a1bc3
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2025-03-24 16:46:29 +0700

    Solution: Count Complete Tree Nodes

--- problem/easy/Count_Complete_Tree_Nodes/Solution.java
+++ problem/easy/Count_Complete_Tree_Nodes/Solution.java
@@ -1,1 +1,39 @@
 package problem.easy.Count_Complete_Tree_Nodes;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+
+        int leftDepth = getDepth(root.left);
+        int rightDepth = getDepth(root.right);
+
+        if(leftDepth == rightDepth) {
+            return (1 << leftDepth) + countNodes(root.right);
+        } else {
+            return (1 << rightDepth) + countNodes(root.left);
+        }
+    }
+
+    public int getDepth(TreeNode node){
+        int count = 0;
+        while(node != null) {
+            count++;
+            node = node.left;
+        }
+
+        return count;
+    }
+}
